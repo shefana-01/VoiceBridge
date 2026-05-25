9894b3849ce44bfdb24c969bfa9296c1c8db8054
shefana-01 <23201147@uap-bd.edu>
2026-05-25 16:42:57 +0600

    Optimize performance

---
+++ voicebridge-backend/boards/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Board, BoardItem
+
+
+class BoardItemInline(admin.TabularInline):
+    model = BoardItem
+    extra = 0
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "child", "rows", "cols",
+                    "is_active", "updated_at")
+    list_filter = ("is_active", "created_at")
+    search_fields = ("name", "owner__username", "child__name")
+    inlines = [BoardItemInline]
+    readonly_fields = ("created_at", "updated_at")
